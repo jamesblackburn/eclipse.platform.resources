7f2dd7dbbb0f6ab279be24cc387752fa0827200d
John Arthorne <johna>
2005-09-29 16:07:05 +0000

    moved resource attribute tests to separate class

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceTest.java
@@ -362,27 +362,6 @@ public class IResourceTest extends ResourceTest implements IFileStoreConstants {
 		return true;
 	}
 
-	private void setArchive(IResource resource, boolean value) throws CoreException {
-		ResourceAttributes attributes = resource.getResourceAttributes();
-		assertNotNull("setAchive for null attributes", attributes);
-		attributes.setArchive(value);
-		resource.setResourceAttributes(attributes);
-	}
-
-	private void setExecutable(IResource resource, boolean value) throws CoreException {
-		ResourceAttributes attributes = resource.getResourceAttributes();
-		assertNotNull("setExecutable for null attributes", attributes);
-		attributes.setExecutable(value);
-		resource.setResourceAttributes(attributes);
-	}
-
-	private void setReadOnly(IResource resource, boolean value) throws CoreException {
-		ResourceAttributes attributes = resource.getResourceAttributes();
-		assertNotNull("setReadOnly for null attributes", attributes);
-		attributes.setReadOnly(value);
-		resource.setResourceAttributes(attributes);
-	}
-
 	protected void setUp() throws Exception {
 		super.setUp();
 		IWorkspaceDescription description = getWorkspace().getDescription();
@@ -655,108 +634,6 @@ public class IResourceTest extends ResourceTest implements IFileStoreConstants {
 			assertTrue("1.4", project2.isSynchronized(IResource.DEPTH_INFINITE));
 		} finally {
 			Workspace.clear(projectPath.toFile());
-		}
-	}
-
-	public void testAttributeArchive() {
-		// archive bit only implemented on windows
-		if (!Platform.getOS().equals(Platform.OS_WIN32))
-			return;
-		IProject project = getWorkspace().getRoot().getProject("Project");
-		IFile file = project.getFile("target");
-		ensureExistsInWorkspace(file, getRandomContents());
-
-		try {
-			// file 
-			// bit is set already for a new file
-			assertTrue("1.0", file.getResourceAttributes().isArchive());
-			setArchive(file, false);
-			assertTrue("1.2", !file.getResourceAttributes().isArchive());
-			setArchive(file, true);
-			assertTrue("1.4", file.getResourceAttributes().isArchive());
-
-			// folder
-			// bit is not set already for a new folder
-			assertTrue("2.0", !project.getResourceAttributes().isArchive());
-			setArchive(project, true);
-			assertTrue("2.2", project.getResourceAttributes().isArchive());
-			setArchive(project, false);
-			assertTrue("2.4", !project.getResourceAttributes().isArchive());
-		} catch (CoreException e1) {
-			fail("2.99", e1);
-		}
-
-		/* remove trash */
-		try {
-			project.delete(true, getMonitor());
-		} catch (CoreException e) {
-			fail("3.0", e);
-		}
-	}
-
-	public void testAttributeExecutable() {
-		// executable bit not implemented on windows
-		if (Platform.getOS().equals(Platform.OS_WIN32))
-			return;
-		IProject project = getWorkspace().getRoot().getProject("Project");
-		IFile file = project.getFile("target");
-		ensureExistsInWorkspace(file, getRandomContents());
-
-		try {
-			// file
-			assertTrue("1.0", !file.getResourceAttributes().isExecutable());
-			setExecutable(file, true);
-			assertTrue("1.2", file.getResourceAttributes().isExecutable());
-			setExecutable(file, false);
-			assertTrue("1.4", !file.getResourceAttributes().isExecutable());
-
-			// folder
-			//folder is executable initially
-			assertTrue("2.0", project.getResourceAttributes().isExecutable());
-			setExecutable(project, false);
-			assertTrue("2.2", !project.getResourceAttributes().isExecutable());
-			setExecutable(project, true);
-			assertTrue("2.4", project.getResourceAttributes().isExecutable());
-		} catch (CoreException e1) {
-			fail("2.99", e1);
-		}
-
-		/* remove trash */
-		try {
-			project.delete(true, getMonitor());
-		} catch (CoreException e) {
-			fail("3.0", e);
-		}
-	}
-
-	public void testAttributeReadOnly() {
-		IProject project = getWorkspace().getRoot().getProject("Project");
-		IFile file = project.getFile("target");
-		ensureExistsInWorkspace(file, getRandomContents());
-
-		try {
-			// file
-			assertTrue("1.0", !file.getResourceAttributes().isReadOnly());
-			setReadOnly(file, true);
-			assertTrue("1.2", file.getResourceAttributes().isReadOnly());
-			setReadOnly(file, false);
-			assertTrue("1.4", !file.getResourceAttributes().isReadOnly());
-
-			// folder
-			assertTrue("2.0", !project.getResourceAttributes().isReadOnly());
-			setReadOnly(project, true);
-			assertTrue("2.2", project.getResourceAttributes().isReadOnly());
-			setReadOnly(project, false);
-			assertTrue("2.4", !project.getResourceAttributes().isReadOnly());
-		} catch (CoreException e1) {
-			fail("2.99", e1);
-		}
-
-		/* remove trash */
-		try {
-			project.delete(true, getMonitor());
-		} catch (CoreException e) {
-			fail("3.0", e);
 		}
 	}
 
